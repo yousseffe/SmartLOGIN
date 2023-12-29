5a30353e2ba0001c8bdd79912b02a6182b0c53b3
Youssef Nabeh <115867801+yousseffe@users.noreply.github.com>
2023-12-29 19:22:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,270 @@
+var Name = document.getElementById('name');
+var Email = document.getElementById('email');
+var Password = document.getElementById('password');
+var LOGINBtn = document.getElementById('LOGINBtn');
+var SIGNUPBtn = document.getElementById('SIGNUPBtn');
+var signup = document.getElementById('SignUP')
+var login = document .getElementById('LogIN')
+var LOGOUTBtn = document.getElementById('LOGOUTBtn');
+var CurUser 
+
+var users= [];
+
+if(localStorage.getItem('users')){
+	users =JSON.parse(localStorage.getItem('users'));
+}
+
+function main(){
+	addUser();
+	resetUP();
+}
+
+
+function addUser(){
+	if(!SIGNUPBtn.hasAttribute("valid-email") && !SIGNUPBtn.hasAttribute("valid-password") && !SIGNUPBtn.hasAttribute("valid-name")){
+		if(Name.value && Email.value && Password.value){
+			var user ={
+				Name: Name.value ,
+				Email : Email.value ,
+				Password : Password.value ,
+			};
+			const userWithEmailExists = users.some(User => User.Email === user.Email);
+			if(!userWithEmailExists){
+				users.push(user);
+				localStorage.setItem('users',JSON.stringify(users));
+				document.getElementById('alert').innerHTML=`<span class=" text-success" style="font-family: 'PT Sans Caption', sans-serif; font-size: larger;">Success</span>`
+			}
+			else{
+				document.getElementById('alert').innerHTML=`<span class=" text-danger" style="font-family: 'PT Sans Caption', sans-serif; font-size: larger;">email already exists</span>`
+			}
+		}
+		
+	}
+}
+
+
+function logIn(){
+	CheckIfExists();
+}
+
+
+
+function CheckIfExists(){
+	if(!LOGINBtn.hasAttribute("valid-email") && !LOGINBtn.hasAttribute("valid-password")){
+		if(Email.value && Password.value){
+			var user ={
+				Email : Email.value ,
+				Password : Password.value ,
+			};
+			const foundUser = users.find(User => User.Email === user.Email);
+
+    		if (foundUser) {
+				if(foundUser.Password === user.Password){
+					CurUser = foundUser.Name; 
+      			SwitchToHome();
+				}
+     			else{
+					document.getElementById('alertIN').innerHTML = `<span class="text-danger" style="font-family: 'PT Sans Caption', sans-serif; font-size: larger;">Incorrect email or password</span>`;
+				}
+    		}
+			else {
+				document.getElementById('alertIN').innerHTML = `<span class="text-danger" style="font-family: 'PT Sans Caption', sans-serif; font-size: larger;">Incorrect email or password</span>`;
+    		}
+		}
+		
+	}
+}
+
+function SwitchToHome(){
+	document.getElementById('main').innerHTML=`
+	<header id="Header" class="vh-100 d-flex justify-content-center align-items-center mb-5">
+      <!-- Start NavBar -->
+      <nav class="navbar navbar-expand-lg bg-transparent fixed-top navbar-dark" id="navbar-example">
+        <div class="container">
+          <h3 class="text-decoration-none link-dark  bold text-white">SMART LOGIN</h3>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav nav-links ms-auto mb-2 mb-lg-0">
+              <li class="nav-item m-2">
+                <button class="btn p-2 border btn-main text-white border-warning btn-main" onclick="SwitchToLogIN()">LOGOUT</button>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- End NavBar -->
+    <!-- Start header -->
+      <div class=" text-center text-white ">
+        <h1 style="font-size: 60px;">Welcome ${CurUser}</h1>
+      </div>
+    </header>
+	`
+}
+function SwitchToSingUP() {
+	document.getElementById('main').innerHTML=`
+	<div class="container vh-100">
+    <h1 class="p-5 text-center" id="body">SMART LOGIN</h1>
+    <div class="w-50 m-auto py-5 px-5 shadow-lg login">
+      <h3 class="mb-5 text-center">SIGNUP NOW</h3>
+      <label><i class="fa-solid fa-user"></i> Name</label>
+      <input type="text" placeholder="Enter your name" id="name" class="form-control my-4" oninput="checkName()" />
+      <label><i class="fa-solid fa-envelope pe-2"></i> Email</label>
+      <input type="email" placeholder="Enter your email" id="email" class="form-control my-4"
+        oninput="checkEmailUP()" />
+      <label><i class="fa-solid fa-lock pe-2"></i>Password</label>
+      <input type="password" placeholder="Enter your password" id="password" class="form-control my-4"
+        oninput="checkPasswordUP()" />
+      <button class="btn btn-primary form-control my-4" id="SIGNUPBtn" onclick="main()">
+        SIGN UP
+      </button>
+      <p>
+        Don’t have an account?
+        <span id="LogIN" onclick="SwitchToLogIN()" style="cursor: pointer; text-decoration: underline">LOGIN</span>
+      </p>
+      <div class="text-center" id="alert"></div>
+    </div>
+  </div>
+	`;
+	Name = document.getElementById('name');
+	Email = document.getElementById('email');
+	Password = document.getElementById('password');
+	SIGNUPBtn = document.getElementById('SIGNUPBtn');
+	signup = document.getElementById('SignUP');
+	
+}
+
+function SwitchToLogIN() {
+	document.getElementById('main').innerHTML=`
+	<div class="container vh-100">
+    <h1 class="p-5 text-center" >SMART LOGIN</h1>
+    <div class="w-50 m-auto py-5 px-5 shadow-lg login">
+      <h3 class="mb-5 text-center">LOGIN NOW</h3>
+        <label><i class="fa-solid fa-envelope pe-2"></i> Email</label>
+        <input type="email" placeholder="Enter your email" id="email" class="form-control my-4" oninput="checkEmail()">
+        <label><i class="fa-solid fa-lock pe-2"></i>Password</label>
+        <input type="password" placeholder="Enter your password" id="password" class="form-control my-4" oninput="checkPassword()">
+        <button class="btn btn-primary form-control my-4" id="LOGINBtn" onclick = "logIn()" >LOG IN</button>
+        <p>Don’t have an account? <span id="SignUP" onclick="SwitchToSingUP()" style="cursor: pointer; text-decoration: underline;" >SIGNUP</span></p>
+        <div class="text-center" id="alertIN"></div>
+    </div>
+  </div>
+	`;
+	Email = document.getElementById('email');
+	Password = document.getElementById('password');
+	login = document .getElementById('LogIN');
+	LOGINBtn = document.getElementById('LOGINBtn');
+}
+var regexName=/^[a-zA-Z1-9]{3,}$/;
+
+function checkName(){
+	
+	if(!regexName.test(Name.value)){
+		SIGNUPBtn.setAttribute("valid-name","NO");
+		Name.classList.add("is-invalid");
+	}else{
+		SIGNUPBtn.removeAttribute("valid-name","NO");
+		Name.classList.add("is-valid");
+        Name.classList.remove("is-invalid");
+	}
+	if(Name.value == ""){
+    	Name.classList.remove("is-valid");
+		Name.classList.remove("is-invalid");
+	}
+	document.getElementById('alert').innerHTML =''
+}
+var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function checkEmail(){
+	if(!regexEmail.test(Email.value)){
+		LOGINBtn.setAttribute("valid-email","NO");
+		
+		Email.classList.add("is-invalid");
+	}else{
+		LOGINBtn.removeAttribute("valid-email","NO");
+		
+		Email.classList.add("is-valid");
+        Email.classList.remove("is-invalid");
+	}
+	if(Email.value == ""){
+    	Email.classList.remove("is-valid");
+		Email.classList.remove("is-invalid");
+	}
+	document.getElementById('alertIN').innerHTML =''
+}
+var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function checkEmailUP(){
+	if(!regexEmail.test(Email.value)){
+		SIGNUPBtn.setAttribute("valid-email","NO");
+		Email.classList.add("is-invalid");
+	}else{
+		SIGNUPBtn.removeAttribute("valid-email","NO");
+		Email.classList.add("is-valid");
+        Email.classList.remove("is-invalid");
+	}
+	if(Email.value == ""){
+    	Email.classList.remove("is-valid");
+		Email.classList.remove("is-invalid");
+	}
+	document.getElementById('alert').innerHTML =''
+
+}
+
+var regexPassword=/^[a-zA-Z1-9]{8,}$/;
+
+function checkPasswordUP(){
+	if(!regexPassword.test(Password.value)){
+		
+		SIGNUPBtn.setAttribute("valid-password","NO");
+		Password.classList.add("is-invalid");
+	}else{
+		
+		SIGNUPBtn.removeAttribute("valid-password","NO");
+		Password.classList.add("is-valid");
+        Password.classList.remove("is-invalid");
+	}
+	if(Password.value == ""){
+    	Password.classList.remove("is-valid");
+		Password.classList.remove("is-invalid");
+	}
+	document.getElementById('alert').innerHTML =''
+
+}
+function checkPassword(){
+	if(!regexPassword.test(Password.value)){
+		LOGINBtn.setAttribute("valid-password","NO");
+		
+		Password.classList.add("is-invalid");
+	}else{
+		LOGINBtn.removeAttribute("valid-password","NO");
+		
+		Password.classList.add("is-valid");
+        Password.classList.remove("is-invalid");
+	}
+	if(Password.value == ""){
+    	Password.classList.remove("is-valid");
+		Password.classList.remove("is-invalid");
+	}
+	document.getElementById('alertIN').innerHTML =''
+}
+function resetUP(){
+	Name.value ="" 
+	Email.value =""
+	Password.value =""
+	Name.classList.remove("is-valid");
+	Name.classList.remove("is-invalid");
+	Email.classList.remove("is-valid");
+	Email.classList.remove("is-invalid");
+	Password.classList.remove("is-valid");
+	Password.classList.remove("is-invalid");
+
+}
+
+function resetIN(){
+	Email.value =""
+	Password.value =""
+	Email.classList.remove("is-valid");
+	Email.classList.remove("is-invalid");
+	Password.classList.remove("is-valid");
+	Password.classList.remove("is-invalid");
+
+}
